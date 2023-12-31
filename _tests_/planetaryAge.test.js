4c9abc36c70ecb7dad0aa259c304f06f638438d1
gene courtney <genecourtney7@gmail.com>
2023-12-31 09:33:03 -0600

    added code to java script and ran first successful test

--- _tests_/planetaryAge.test.js
+++ _tests_/planetaryAge.test.js
@@ -1,10 +1,12 @@
-import Planet from "../src/planetaryAge";
+import SpaceAge from "../src/planetaryAge";
 
-describe('Planet', () => {
-    const myPlanet = new Planet ();
+describe('SpaceAge', () => {
+    const myAge = new SpaceAge(26);
     const earthAge = 26;
 
     test('should correctly calculate age on Mercury', () => {
-        const expectedAge = earthAge / 0.241;
-        expect(myPlanet.calculateAge(earthAge,"Mercury")).toBeCloseTo(expectedAge);
+        const expectedAge = earthAge / 0.24;
+        expect(myAge.onPlanet('Mercury')).toBeCloseTo(expectedAge);
     });
+
+});
--- src/planetaryAge.js
+++ src/planetaryAge.js
@@ -1,16 +1,24 @@
-export default class Planet {
-    constructor(age) {
-        this.age = age;
-        this.planetYears = {
-            "Mercury": 0.241,
-            "Venus": 0.615,
-            "Earth": 1,
-            "Mars": 1.881,
-            "Jupiter": 11.86,
-            "Saturn": 29.46,
-            "Uranus": 84.07,
-            "Neptune": 164.8,
-        };
-    }
+export default class SpaceAge {
+  constructor(age) {
+    this.age = age;
+    this.planetYears = {
+      'Earth': 1,
+      'Mercury': 0.24,
+      'Venus': 0.62,
+      'Mars': 1.88,
+      'Jupiter': 11.86,
+    };
+  }
 
-  
+  onPlanet(planet) {
+    return Number((this.age / this.planetYears[planet]).toFixed(2));
+  }
+
+  yearsPassed(planet, pastBirthday) {
+    return Number(((this.age - pastBirthday) / this.planetYears[planet]).toFixed(2));
+  }
+
+  yearsToCome(planet, futureBirthday) {
+    return Number(((futureBirthday - this.age) / this.planetYears[planet]).toFixed(2));
+  }
+}
