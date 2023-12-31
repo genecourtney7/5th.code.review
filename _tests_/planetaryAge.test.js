5a51bce4386d32c07cebdef2b7c3eb4fe82ca8ea
gene courtney <genecourtney7@gmail.com>
2023-12-31 09:45:13 -0600

    added second test: success, displays age for venus.

--- _tests_/planetaryAge.test.js
+++ _tests_/planetaryAge.test.js
@@ -1,12 +1,18 @@
 import SpaceAge from "../src/planetaryAge";
 
 describe('SpaceAge', () => {
-    const myAge = new SpaceAge(26);
-    const earthAge = 26;
+  const myAge = new SpaceAge(26);
+  const earthAge = 26;
 
-    test('should correctly calculate age on Mercury', () => {
-        const expectedAge = earthAge / 0.24;
-        expect(myAge.onPlanet('Mercury')).toBeCloseTo(expectedAge);
-    });
+  test('should correctly calculate age on Mercury', () => {
+    const expectedAge = earthAge / 0.24;
+    expect(myAge.onPlanet('Mercury')).toBeCloseTo(expectedAge);
+  });
 
+  test('should correctly calculate age on Venus', () => {
+    const expectedAge = earthAge / 0.62;
+    expect(myAge.onPlanet('Venus')).toBeCloseTo(expectedAge);
+  });
+
+    
 });
