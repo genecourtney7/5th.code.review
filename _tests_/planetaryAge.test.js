5a42b4a771695924a0959dd5f484555763e79e75
gene courtney <genecourtney7@gmail.com>
2023-12-31 23:35:34 -0600

    wrote the remainder of the tests for the plantes

--- _tests_/planetaryAge.test.js
+++ _tests_/planetaryAge.test.js
@@ -54,10 +54,78 @@ describe('SpaceAge', () => {
     expect(myAge.yearsPassed('Mercury', 20)).toBeCloseTo(expectedAge); 
   });
 
+  test('should correctly calculate years passed on Venus since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 0.62;
+    expect(myAge.yearsPassed('Venus', 20)).toBeCloseTo(expectedAge);
+  });
 
+  test('should correctly calculate years passed on Mars since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 1.88;
+    expect(myAge.yearsPassed('Mars', 20)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years passed on Jupiter since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 11.86;
+    expect(myAge.yearsPassed('Jupiter', 20)).toBeCloseTo(expectedAge);
+  });
+  
+  test('should correctly calculate years passed on Saturn since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 29.46;
+    expect(myAge.yearsPassed('Saturn', 20)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years passed on Uranus since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 84.01;
+    expect(myAge.yearsPassed('Uranus', 20)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years passed on Neptune since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 164.8;
+    expect(myAge.yearsPassed('Neptune', 20)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years passed on Pluto since 20th birthday', () => {
+    const expectedAge = (earthAge - 20) / 248.00;
+    expect(myAge.yearsPassed('Pluto', 20)).toBeCloseTo(expectedAge);
+  });
 
   test('should correctly calculate years to come on Mercury until 50th Birthday', () => {
     const expectedAge = (50 - earthAge) / 0.24;
     expect(myAge.yearsToCome('Mercury', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Venus until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 0.62;
+    expect(myAge.yearsToCome('Venus', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Mars until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 1.88;
+    expect(myAge.yearsToCome('Mars', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Jupiter until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 11.86;
+    expect(myAge.yearsToCome('Jupiter', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Saturn until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 29.46;
+    expect(myAge.yearsToCome('Saturn', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Uranus until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 84.01;
+    expect(myAge.yearsToCome('Uranus', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Neptune until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 164.8;
+    expect(myAge.yearsToCome('Neptune', 50)).toBeCloseTo(expectedAge);
+  });
+
+  test('should correctly calculate years to come on Pluto until 50th birthday', () => {
+    const expectedAge = (50 - earthAge) / 248.00;
+    expect(myAge.yearsToCome('Pluto', 50)).toBeCloseTo(expectedAge);
   });
 });
